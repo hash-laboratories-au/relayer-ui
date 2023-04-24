5e900a956af870e1bf8b292ce7b2c603d1906743
Jianrong <wjrjerome@gmail.com>
2023-04-25 00:22:34 +1000

    new SC address

--- src/services/mainnet.ts/index.ts
+++ src/services/mainnet.ts/index.ts
@@ -3,7 +3,7 @@ import { abi } from '../utils';
 import { extensions } from "../extensions";
 
 const mainnetUrl = "https://devnetstats.apothem.network/mainnet";
-const smartContractAddress = "0x0552B96871fA181dD2218035762353bfa26778c1";
+const smartContractAddress = "0x4DcbE9858E5d3fc14B5B57B6f01934B3ec16b421";
 
 const web3Client = new web3(mainnetUrl).extend(extensions);
 const smartContractInstance = new web3Client.eth.Contract(abi as any, smartContractAddress);
